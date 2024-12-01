74a01ec960dde433437a459184fabdf68a113d84
ngynzyy <thuyvynguyen42@gmail.com>
2024-12-01 19:39:52 +0700

    chore: TARP-20 Format SQL code

--- models/staging/base/base_tripadvisor__geolocation.sql
+++ models/staging/base/base_tripadvisor__geolocation.sql
@@ -6,24 +6,24 @@ WITH cte__geolocation__source_name AS (
 )
 
 , cte__geolocation__rename_column AS (
-    SELECT 
-        district_name 
-        ,district_codename AS district_code 
-        ,ward_name
-        ,ward_codename AS ward_code 
-        ,latitude
-        ,longitude
+    SELECT
+        district_name
+        , district_codename AS district_code
+        , ward_name
+        , ward_codename AS ward_code
+        , latitude
+        , longitude
     FROM cte__geolocation__source_name
 )
 
 , cte__geolocation__change_type AS (
-    SELECT 
-        CAST(district_name AS STRING) district_name
-        ,CAST(district_code AS STRING) district_code
-        ,CAST(ward_name AS STRING) ward_name
-        ,CAST(ward_code AS STRING) ward_code
-        ,CAST(latitude AS NUMERIC) latitude
-        ,CAST(longitude AS NUMERIC) longitude
+    SELECT
+        CAST(district_name AS STRING) AS district_name
+        , CAST(district_code AS STRING) AS district_code
+        , CAST(ward_name AS STRING) AS ward_name
+        , CAST(ward_code AS STRING) AS ward_code
+        , CAST(latitude AS NUMERIC) AS latitude
+        , CAST(longitude AS NUMERIC) AS longitude
     FROM cte__geolocation__rename_column
 )
 
