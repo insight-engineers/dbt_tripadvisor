ac739b13572b9303724af2c259bd3b323b083e2a
thangbuiq <buiquangthangtv@gmail.com>
2024-12-01 10:40:05 +0700

    chore: remove python model

--- models/staging/stg_tripadvisor__queries.py
+++
@@ -1,9 +0,0 @@
-import dateutil
-import pandas
-
-
-def model(dbt, session):
-    df = dbt.ref("base_tripadvisor__queries").to_df()
-
-    ## TODO: Perform some transformations on the data
-    return df
